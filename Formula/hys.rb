9c26063553de66e1a270ed0ad45f7243b8bf549b
superstarryeyes <superstarryeyes@gmail.com>
2025-12-16 15:52:46 +0200

    Hys formula

---
+++ Formula/hys.rb
@@ -0,0 +1,21 @@
+class Hys < Formula
+  desc "RSS Reader for Digital Minimalists"
+  homepage "https://github.com/superstarryeyes/hys"
+  license "MIT"
+  
+  url "https://github.com/superstarryeyes/hys/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  version "0.1.0"
+
+  depends_on "zig" => :build
+  depends_on "curl"
+  depends_on "pkg-config" => :build
+
+  def install
+    system "zig", "build", "install", "--prefix", prefix, "-Doptimize=ReleaseSafe"
+  end
+
+  test do
+    assert_match "hys", shell_output("#{bin}/hys --version")
+  end
+end
