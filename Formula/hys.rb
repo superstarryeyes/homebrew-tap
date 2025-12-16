a394674fb1e22a0021ed13082cde65830617da7c
superstarryeyes <superstarryeyes@gmail.com>
2025-12-16 16:09:21 +0200

    Fix sha256 checksum for hys formula

--- Formula/hys.rb
+++ Formula/hys.rb
@@ -2,7 +2,7 @@ class Hys < Formula
   desc "RSS Reader for Digital Minimalists"
   homepage "https://github.com/superstarryeyes/hys"
   url "https://github.com/superstarryeyes/hys/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+  sha256 "137cc1c2ace93fc34c124b22266fc7f2f2279a048f4005c559ccbe9eb29a01f7"
   license "MIT"
 
   depends_on "pkg-config" => :build
