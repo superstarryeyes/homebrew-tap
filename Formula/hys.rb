edbba5eb20c4c2810b0e0faf8494153f802c589f
superstarryeyes <superstarryeyes@gmail.com>
2025-12-16 16:00:17 +0200

    Fix hys formula formatting and metadata order

--- Formula/hys.rb
+++ Formula/hys.rb
@@ -1,11 +1,9 @@
 class Hys < Formula
   desc "RSS Reader for Digital Minimalists"
   homepage "https://github.com/superstarryeyes/hys"
-  license "MIT"
-  
   url "https://github.com/superstarryeyes/hys/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
-  version "0.1.0"
+  license "MIT"
 
   depends_on "zig" => :build
   depends_on "curl"
